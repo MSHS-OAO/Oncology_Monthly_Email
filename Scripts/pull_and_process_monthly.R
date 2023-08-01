d0f38d7605302f83086dcbc552cb8a1135d29902
armando-v <>
2023-08-01 12:13:05 -0400

    update writing of csv

--- Scripts/pull_and_process_monthly.R
+++ Scripts/pull_and_process_monthly.R
@@ -16,10 +16,16 @@ month_year <- format(last_month, '%Y-%m')
 sql_statement <- glue("SELECT * FROM ONCOLOGY_ACCESS WHERE APPT_MONTH_YEAR = '{month_year}' AND APPT_STATUS = 'Arrived'")
 previous_month_data <- dbGetQuery(conn, sql_statement)
 
-save_uncompressed_file_path <- paste0("/SharedDrive/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Oncology/Monthly Oncology Email/Uncompressed/Oncology_Arrived_Data_",
+save_uncompressed_file_path <- paste0("/SharedDrive/deans/Presidents/HSPI-PM/Operations Analytics and Optimization/Projects/Service Lines/Oncology/Monthly Oncology Email/Uncompressed/Oncology Arrived Data ",
                          month_year, " saved ", format(Sys.time(), "%Y-%m-%d %H.%M"), ".csv")
 
-write_csv(previous_month_data, save_uncompressed_file_path)
+#Using write.table becasue it allows us to have blanks instead of NA and preserver timestamp format
+write.table(previous_month_data, save_uncompressed_file_path,
+            na = "",
+            row.names = FALSE,
+            col.names = TRUE,
+            append = FALSE,
+            sep = ",")
 
 save_compressed_file_path <- gsub("\\<Uncompressed\\>","Compressed",save_uncompressed_file_path)
 save_compressed_file_path <- gsub("\\<csv\\>","zip",save_compressed_file_path)
